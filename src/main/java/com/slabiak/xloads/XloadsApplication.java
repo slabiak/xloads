7a29885e181c89fbca538fd88b0eea2d6192598a
Tomasz <slabiak.tomasz@gmail.com>
2019-09-02 20:35:36 +0200

    init commit

---
+++ src/main/java/com/slabiak/xloads/XloadsApplication.java
@@ -0,0 +1,13 @@
+package com.slabiak.xloads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XloadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XloadsApplication.class, args);
+	}
+
+}
