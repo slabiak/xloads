e105f704796e945077a07fcea4fae175685751a5
slabiak <slabiak.tomasz@gmail.com>
2019-10-06 17:17:14 +0200

    configure cors

--- src/main/java/com/slabiak/xloads/configuration/AppConfig.java
+++ src/main/java/com/slabiak/xloads/configuration/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfig {
@@ -27,12 +28,10 @@ public class AppConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
